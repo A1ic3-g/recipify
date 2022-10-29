0503aec804ab4b472eeb6c638f28b47cedda6b61
Carnagion <carnagion.games@gmail.com>
2022-10-29 20:05:52 +0100

    Refactor recommendation and playlist querying functions

--- src/spotify.py
+++ src/spotify.py
@@ -1,5 +1,7 @@
 import dotenv
+import fuzzysearch
 import os
+import random
 from spotipy import *
 
 from typing import *
@@ -19,33 +21,61 @@ spotify: Spotify = Spotify(
 )
 
 
-def recommend(recipe: Dict[str, Any]) -> Any:
+def recommend(recipe: Dict[str, Any]) -> List[Dict]:
     """
-    Recommend a Spotify track based on recipe data.
+    Recommend playlists from Spotify based on a recipe.
 
-    :param recipe:
-    :return:
+    :param recipe: The recipe.
+    :return: A list of playlist recommendations.
     """
 
     # List of recommendable Spotify genres
     genres: List[str] = spotify.recommendation_genre_seeds()["genres"]
 
-    # Recipe health and dish types
-    healths: List[str] = recipe["healthLabels"]
+    recommendations = []
+
+    # Retrieve playlists matching genres
+    cuisines: List[str] = recipe["cuisines"]
+    for cuisine in cuisines:
+        for genre in genres:
+            fuzzy: List[fuzzysearch.Match] = fuzzysearch.find_near_matches(subsequence=genre, sequence=cuisine, max_l_dist=1)
+            match fuzzy:
+                case [_]:
+                    recommendations.extend(playlists(query=genre))
+
+    # Retrieve playlists matching main meal type
+    meals: List[str] = recipe["mealType"]
+    match meals:
+        case [meal]:
+            recommendations.extend(playlists(query=meal))
+
+    # Retrieve playlists matching dish types
     dishes: List[str] = recipe["dishType"]
+    recommendations.extend([playlists(query=dish) for dish in dishes])
 
-    health_genres: List[str] = healths_to_genres(healths, genres)
+    # Special cases
 
-    return spotify.recommendations(
-        seed_genres=genres[:5],
-        limit=10,
-    )
+    # Return only pirate metal playlists for alcoholic caribbean drinks
+    if ("alcohol-cocktail" in recipe["healthLabels"]) and ("caribbean" in cuisines):
+        recommendations = random.choice(playlists(query="yo ho ho and a bottle of rum", limit=50))
 
-
-def healths_to_genres(healths: List[str], genres: List[str]) -> List[str]:
-    match healths:
-        case ["alcohol-cocktail"]:
-            return []
+    return recommendations
 
 
-print([track["name"] for track in recommend({})["tracks"]])
+def playlists(query: str, limit: int = 1) -> List[Dict]:
+    """
+    Retrieve playlists from Spotify based on a query string.
+
+    :param query: The query to use when searching for playlists.
+    :param limit: The limit on the number of playlists returned.
+    :return: A list of playlists matching the specified query.
+    """
+    return spotify.search(q=query, type="playlist", limit=limit)["playlists"]["items"]
+
+
+print(recommend({
+    "cuisines": ["caribbean"],
+    "healthLabels": ["alcohol-cocktail"],
+    "mealType": [],
+    "dishType": [],
+}))
