214bc52f6395693ab4b5d85e708ada1efbbad3a8
Dom Binks <dominic.binks@gmail.com>
2022-10-29 23:59:22 +0100

    Get app.py queries working

--- recipify/app.py
+++ recipify/app.py
@@ -1,10 +1,15 @@
-
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for
 from flask_bootstrap import Bootstrap
+from src.edamam import get_recipes
+import json
 
 app = Flask(__name__)
 Bootstrap(app)
 
+@app.route('/', methods=['POST'])
+def index_post():
+    query = json.loads(get_recipes(request.form['query']))
+    return "<h1>{0}</h1>".format(query)
 
 @app.route('/')
 def index_page():
