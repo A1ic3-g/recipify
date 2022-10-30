ac2fa9a134fada1c783f416a4dc83f011b2228ee
Dom Binks <dominic.binks@gmail.com>
2022-10-30 06:56:43 +0000

    Fix box height on recipe page

--- recipify/app.py
+++ recipify/app.py
@@ -97,8 +97,7 @@ def recipe_page():
 
     return render_template(
         "recipe.html",
-        spotify_link=playlists[0]['id'], # The best playlist
-        #spotify_link="38u384bGcqsSEWtub2XbEF",
+        spotify_link=random.choice(playlists)['id'],
         recipe_link=url,
         label=label
     )
