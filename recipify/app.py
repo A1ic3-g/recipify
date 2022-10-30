03f2e72b8ea320cc3b60179be5a29a191f7b85a9
Dom Binks <dominic.binks@gmail.com>
2022-10-30 01:11:36 +0100

    Make progress on results.html

--- recipify/app.py
+++ recipify/app.py
@@ -9,7 +9,10 @@ Bootstrap(app)
 @app.route('/', methods=['POST'])
 def index_post():
     query = json.loads(get_recipes(request.form['query']))
-    return "<h1>{0}</h1>".format(query)
+    #return "<h1>{0}</h1>".format(query)
+    return render_template("results.html", image=query[0]['image'], url=query[0]['url'],
+        label=query[0]['label'], mealType=query[0]['mealType'][0], cuisine=query[0]['cuisineType'][0],
+        calories=query[0]['calories'])
 
 @app.route('/')
 def index_page():
